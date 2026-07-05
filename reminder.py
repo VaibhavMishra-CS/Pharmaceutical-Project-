2ceeec4f4c72f06da5f9eed7c4473d9f59b65bfa
Vaibhav Mishra <mkrrish575@gmail.com>
2026-07-05 06:42:21 +0000

    printing messages command

--- reminder.py
+++ reminder.py
@@ -10,3 +10,8 @@ load_dotenv()
 account_sid = os.getenv('TWILIO_ACCOUNT_SID')
 auth_token = os.getenv('TWILIO_AUTH_TOKEN')
 client = client(account_sid, auth_token)
+# 3. functions 
+def send_reminder(patient_name,medicine): 
+    print("Reminder triggered!")
+    print(f"Sending messages to {patient_name}...")
+    
