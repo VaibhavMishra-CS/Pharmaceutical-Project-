12de8d34dc66f14639bda40fc332a82adb4991ab
Vaibhav Mishra <mkrrish575@gmail.com>
2026-07-05 07:53:07 +0000

    completed the model project

--- reminder.py
+++ reminder.py
@@ -1,16 +1,22 @@
 import os 
 from dotenv import load_dotenv 
 from twilio.rest import Client
+from twilio.base.exceptions import TwilioRestException
 import schedule 
 import time 
 
+patient_name = "ron"
+medicine = "paracetamol"
+scheduled_time = "10:00" 
+
 # 1. load credentials 
 load_dotenv()
+print(f"SIS found: {os.getenv('TWILIO_ACCOUNT_SID')}, {os.getenv('TWILIO_AUTH_TOKEN')}, {os.getenv('Patient_number')}")
 # 2. Twilio 
 account_sid = os.getenv('TWILIO_ACCOUNT_SID')
 auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-patient_number = os.getenv('patient_number')
-client = client(account_sid, auth_token)
+patient_number = os.getenv('Patient_number')
+client = Client(account_sid, auth_token)
 # 3. functions 
 def send_reminder(patient_name,medicine): 
     """Sends a formatted medication reminder via Twilio"""
@@ -31,7 +37,7 @@ except TwilioRestException as e:
     print(f"An error occurred: {e}")
 
 # scheduling the task
-schedule.every().day.at("scheduled_time").do(send_reminder, patient_name, medicine)
+schedule.every().day.at(scheduled_time).do(send_reminder, patient_name, medicine)
 
 #loop
 print("system is running and waiting for schedule...")
