dfae307393a4079034eb64bed8f47ad97ddf5d5f
Vaibhav Mishra <mkrrish575@gmail.com>
2026-07-05 07:22:14 +0000

    added error detection

--- reminder.py
+++ reminder.py
@@ -13,7 +13,9 @@ patient_number = os.getenv('patient_number')
 client = client(account_sid, auth_token)
 # 3. functions 
 def send_reminder(patient_name,medicine): 
-    print("Reminder triggered!")
+    """Sends a formatted medication reminder via Twilio"""
+
+try:
     print(f"Sending messages to {patient_name}...")
 
     #implementing 'Messaging_service_sid'
@@ -23,3 +25,7 @@ def send_reminder(patient_name,medicine):
         to=patient_number
     )
     print(f"Success! Message sent to {patient_name}. SID: {message.sid}")
+
+    #ERRORS
+except TwilioRestException as e:
+    print(f"An error occurred: {e}")
