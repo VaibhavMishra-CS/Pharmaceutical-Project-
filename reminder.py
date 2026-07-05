b78519126145da3804c8b04601d4c57101b88b0d
Vaibhav Mishra <mkrrish575@gmail.com>
2026-07-05 06:58:23 +0000

    Final message to patient

--- reminder.py
+++ reminder.py
@@ -9,9 +9,17 @@ load_dotenv()
 # 2. Twilio 
 account_sid = os.getenv('TWILIO_ACCOUNT_SID')
 auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+patient_number = os.getenv('patient_number')
 client = client(account_sid, auth_token)
 # 3. functions 
 def send_reminder(patient_name,medicine): 
     print("Reminder triggered!")
     print(f"Sending messages to {patient_name}...")
-    
+
+    #implementing 'Messaging_service_sid'
+    message = client.messages.create(
+        body=f"Hello {patient_name}, it is time for your {medicine}",
+        from_='SMART MEDICATOR', 
+        to=patient_number
+    )
+    print(f"Success! Message sent to {patient_name}. SID: {message.sid}")
