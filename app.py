47b84d87a0da6c17c6cd5b9e3fb82826761f2644
Vaibhav Mishra <mkrrish575@gmail.com>
2026-07-05 09:19:19 +0000

    listed number of medications,dosage per day

--- app.py
+++ app.py
@@ -7,17 +7,39 @@ st.title("Pharmacy Medication Reminder System")
 #intial session state to keep track of added medications 
 if 'medications' not in st.session_state:
     st.session_state.medications = []
-
+# Input section
 with st.form("patient_form"):
     patient_name = st.text_input("Enter Patient Name")
+    dosage = st.text_input("Enter Dosage (mg/ml), times per day=1")
     medicine = st.text_input("Enter Medicine Name")
     scheduled_time = st.time_input("Enter Scheduled Time")
     phone_number = st.text_input("Enter Patient Phone Number")
     submit_button = st.form_submit_button("Submit")
 
     if submit_button:
+        st.session_state.medications.append({
+            "patient_name": patient_name,
+            "dosage": dosage,
+            "medicine": medicine,
+            "scheduled_time": scheduled_time,
+            "phone_number": phone_number
+        })
+
+# Display the list of added medications
+st.subheader("Your Daily Schedule")
+for i, medicine in enumerate(st.session_state.medications):
+    st.write(f"{i+1}. {medicine['medicine']} ({medicine['dosage']}mg/ml) at {medicine['scheduled_time']}")
+
+#final submit
+    if submit_button("Save Full Schedule"):
         # Save the data to a CSV file
         with open("patient_data.csv", mode="a", newline="") as file:
             writer = csv.writer(file)
+            for med in st.session_state.medications:
+                patient_name = med['patient_name']
+                medicine = med['medicine']
+                scheduled_time = med['scheduled_time']
+                phone_number = med['phone_number']
             writer.writerow([patient_name, medicine, scheduled_time, phone_number])
         st.success("Patient Enrolled successfully!")
+        st.session_state.medications = [] #clear list
