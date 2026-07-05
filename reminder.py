96550a6aee9c9dad14f1b862f1b06955603494a3
Vaibhav Mishra <mkrrish575@gmail.com>
2026-07-05 06:17:02 +0000

    authentication of twilio

--- reminder.py
+++ reminder.py
@@ -4,4 +4,9 @@ from twilio.rest import Client
 import schedule 
 import time 
 
+# 1. load credentials 
 load_dotenv()
+# 2. Twilio 
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+client = client(account_sid, auth_token)
