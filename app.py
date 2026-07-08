8f87f83850bde8f6a83e3a6d0a9363a488340c51
Vaibhav Mishra <mkrrish575@gmail.com>
2026-07-08 05:00:35 +0000

    added colomns

--- app.py
+++ app.py
@@ -2,35 +2,42 @@ import streamlit as st
 import csv 
 import os 
 
+st.set_page_config(layout="wide") #making website wider
 st.title("Pharmacy Medication Reminder System")
+
+#create two columns 
+col1, col2 = st.columns([1, 1])
 
 #intial session state to keep track of added medications 
 if 'medications' not in st.session_state:
     st.session_state.medications = []
-# Input section
-with st.form("patient_form"):
-    patient_name = st.text_input("Enter Patient Name")
-    dosage = st.text_input("Enter Dosage (mg/ml)") 
-    times_per_day = st.number_input("Enter Number of Times per Day", min_value=1, max_value=10, step=1)
-    medicine = st.text_input("Enter Medicine Name")
-    scheduled_time = st.time_input("Enter Scheduled Time")
-    phone_number = st.text_input("Enter Patient Phone Number")
-    submit_button = st.form_submit_button("Submit")
 
-    if submit_button:
-        st.session_state.medications.append({
-            "patient_name": patient_name,
-            "dosage": dosage,
-            "times_per_day": times_per_day,
-            "medicine": medicine,
-            "scheduled_time": scheduled_time,
-            "phone_number": phone_number
-        })
+# Input section
+with col1:
+    with st.form("patient_form"):
+        patient_name = st.text_input("Enter Patient Name")
+        dosage = st.text_input("Enter Dosage (mg/ml)") 
+        times_per_day = st.number_input("Enter Number of Times per Day", min_value=1, max_value=10, step=1)
+        medicine = st.text_input("Enter Medicine Name")
+        scheduled_time = st.time_input("Enter Scheduled Time")
+        phone_number = st.text_input("Enter Patient Phone Number")
+        submit_button = st.form_submit_button("Submit")
+
+        if submit_button:
+             st.session_state.medications.append({
+                 "patient_name": patient_name,
+                 "dosage": dosage,
+                 "times_per_day": times_per_day,
+                 "medicine": medicine,
+                 "scheduled_time": scheduled_time,
+                "phone_number": phone_number
+          })
 
 # Display the list of added medications
-st.subheader("Your Daily Schedule")
-for i, medicine in enumerate(st.session_state.medications):
-    st.write(f"{i+1}. {medicine['medicine']} ({medicine['dosage']}mg/ml) at {medicine['scheduled_time']}")
+with col2:
+    st.subheader("Your Daily Schedule")
+    for i, medicine in enumerate(st.session_state.medications):
+        st.write(f"{i+1}. {medicine['medicine']} ({medicine['dosage']}mg/ml) at {medicine['scheduled_time']}")
 
 #final submit
     if st.button("Save Full Schedule", key='save_button'):
